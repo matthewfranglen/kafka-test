7a701188a792eb888458f2c2b83a9a7cff3b0680
Matthew Franglen <matthew@franglen.org>
2017-04-19 23:40:40 +0100

    Add logging

--- src/main/java/com/matthew/KafkaTestConfiguration.java
+++ src/main/java/com/matthew/KafkaTestConfiguration.java
@@ -4,6 +4,8 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,13 +17,15 @@ import com.matthew.services.Writer;
 @Configuration
 public class KafkaTestConfiguration {
 
-    public static final String MESSAGE = "The Magic Words are Squeamish Ossifrage";
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Bean
     public Writer writer(
             @Value("${kafka.server}") String server,
             @Value("${kafka.topic}") String topic
     ) {
+        logger.info("Creating Writer: {}, {}", server, topic);
+
         return new Writer(server, topic);
     }
 
@@ -31,6 +35,8 @@ public class KafkaTestConfiguration {
             @Value("${kafka.topic}") String topic,
             @Value("${kafka.consumerGroup}") String consumerGroup
     ) {
+        logger.info("Creating Reader: {}, {}, {}", server, topic, consumerGroup);
+
         return new Reader(server, topic, consumerGroup);
     }
 
@@ -40,6 +46,8 @@ public class KafkaTestConfiguration {
             @Value("${kafka.topic}") String topic,
             @Value("${kafka.consumerGroup}") String consumerGroup
     ) {
+        logger.info("Creating Monitor: {}, {}, {}", server, topic, consumerGroup);
+
         return new Monitor(server, topic, consumerGroup);
     }
 
